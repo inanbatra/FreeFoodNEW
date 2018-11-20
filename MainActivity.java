00ef1bc95d21a82f05eb2a4b981c0e0f66cfed9d
Inan Batra <inanbatra@gmail.com>
2018-11-19 21:41:57 -0500

    Initial

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.inanbatra.freefoodnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// hello this is the new app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
